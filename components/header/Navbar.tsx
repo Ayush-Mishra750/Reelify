03f6c3385ce26b2127f224cb83009ffa20fa850f
mishra Ji <ayushmishra270306@gmail.com>
2025-08-02 16:00:25 +0530

    initial commit

---
+++ components/header/Navbar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Input } from "../ui/input";
+import { Plus } from "lucide-react";
+import { Button } from "../ui/button";
+import { ModeToggle } from "../ModeToggle";
+
+const Navbar = () => {
+  return (
+  <div className="flex items-center justify-between px-4 h-14 gap-4">
+  {/* logo */}
+  <div className="flex-shrink-0">
+    <h1 className="text-xl font-bold">
+      YT <span className="text-red-500">Shorts</span>
+    </h1>
+  </div>
+
+  {/* search bar */}
+  <div className="flex-grow max-w-xl">
+    <Input type="text" placeholder="Search ..." className="w-full" />
+  </div>
+
+  {/* icons */}
+  <div className="flex items-center space-x-2">
+    <Button>
+      <Plus className="mr-1" />
+      Create
+    </Button>
+    <ModeToggle />
+  </div>
+</div>
+
+  );
+};
+
+export default Navbar;
